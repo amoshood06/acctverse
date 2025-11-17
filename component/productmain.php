a9ebc261c7956ad0e2029f9c8f0449ebd3ce7d53
amoshood06 <amoshood06@gmail.com>
2025-11-17 15:10:39 +0100

    product

--- component/productmain.php
+++ component/productmain.php
@@ -1,4 +1,4 @@
-<div class="max-w-2xl mx-auto p-4 md:p-6">
+<div class="acctverse_product p-4 md:p-6">
         <!-- Header Section -->
         <div class="flex flex-col md:flex-row md:justify-between md:items-center gap-3 mb-6">
             <h1 class="text-lg sm:text-xl md:text-2xl font-bold text-white bg-red-700 px-4 py-2 rounded-lg whitespace-normal break-words inline-block">
